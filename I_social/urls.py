b739472c6c20bdae528ebbaff82403a8fe503e0b
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-06-20 15:38:13 +0100

    changes

--- I_social/urls.py
+++ I_social/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for I_social project.
+"""social_book URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,10 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
 ]
+
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
